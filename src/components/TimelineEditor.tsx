adbf5aeabc6b10b019c04f4875de7fbeee75b7e4
Nahom Bekele <nahom.fix@gmail.com>
2024-04-01 11:34:47 +0300

    wip: add layers on timeline

--- src/components/TimelineEditor.tsx
+++ src/components/TimelineEditor.tsx
@@ -1,14 +1,100 @@
-import { Timeline } from "@xzdarcy/react-timeline-editor";
-import { FC } from "react";
+import { Timeline, TimelineRow } from "@xzdarcy/react-timeline-editor";
+import { FC, useState } from "react";
+import { useEditorStore } from "../store";
+
+const mockEffects = {
+    effect0: {
+        id: "effect-0",
+        name: "效果0",
+    },
+    effect1: {
+        id: "effect-1",
+        name: "效果1",
+    },
+};
+
+// const mockData = [
+//     {
+//         id: "0",
+//         actions: [
+//             {
+//                 id: "action00",
+//                 start: 0,
+//                 end: 2,
+//                 effectId: "effect0",
+//             },
+//         ],
+//     },
+//     {
+//         id: "1",
+//         actions: [
+//             {
+//                 id: "action10",
+//                 start: 1.5,
+//                 end: 5,
+//                 effectId: "effect1",
+//             },
+//         ],
+//     },
+//     {
+//         id: "2",
+//         actions: [
+//             {
+//                 id: "action20",
+//                 flexible: false,
+//                 movable: false,
+//                 start: 3,
+//                 end: 4,
+//                 effectId: "effect0",
+//             },
+//         ],
+//     },
+//     {
+//         id: "3",
+//         actions: [
+//             {
+//                 id: "action30",
+//                 start: 4,
+//                 end: 4.5,
+//                 effectId: "effect1",
+//             },
+//             {
+//                 id: "action31",
+//                 start: 6,
+//                 end: 8,
+//                 effectId: "effect1",
+//             },
+//         ],
+//     },
+// ];
 
 export const TimelineEditor: FC = () => {
+    const timeline = useEditorStore((state) => state.timeline);
+    const timelineData: TimelineRow[] = timeline.map((layer, index) => ({
+        id: `${index}`,
+        actions: [
+            {
+                id: `${index}`,
+                start: layer.startTime,
+                end: layer.endTime,
+                effectId: `effect-${index}`,
+            },
+        ],
+    }));
+    const [data, setData] = useState(timelineData);
+
     return (
-        <Timeline
-            style={{
-                width: "auto",
-            }}
-            editorData={[]}
-            effects={{}}
-        />
+        <>
+            <Timeline
+                style={{
+                    width: "auto",
+                }}
+                editorData={data}
+                effects={mockEffects}
+                onChange={setData}
+                autoScroll
+                hideCursor={false}
+            />
+        </>
     );
 };
